7d0133920f579f23ea1db79b8277b233c0c511b0
Daniel Lara <daniel.lara.diezma@gmail.com>
2017-10-15 00:11:08 +0200

    Creates simple messenger structure

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-let io = socketIO(server);
+const io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 
@@ -14,9 +14,28 @@ app.use(express.static(path.join(__dirname, '../public')));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('welcomeMessage', {
+        from: 'Admin',
+        text: 'Welcome to Hi-chat'
+    });
+
+    socket.broadcast.emit('welcomeMessage', {
+        from: 'Admin',
+        text: 'An user has join to the chat'
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('New message received:', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: Date.now()
+        });
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected');
-    })
+    });
 });
 
 server.listen(port, () => {
